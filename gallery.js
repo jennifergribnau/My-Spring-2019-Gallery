fd9005dcccc4d3729c0696e4c3c1d04a98f32b08
Jennifer Gribnau <jeg294@georgetown.edu>
2019-02-12 19:18:54 -0500

    outlining JavaScript

--- gallery.js
+++ gallery.js
@@ -1,1 +1,60 @@
-//document ready
+// document ready function
+
+	// DEFINE YOUR VARIABLES
+	// select all the slides (value of the variable will be an array)
+
+	// set the ndex slide (number) AKA where we start
+
+	// current slide based on the index
+
+	// STEPS
+	// 1. hide all slides except for the one we want to see (e.g all but the first)
+			// a.
+			// b.
+	// 2. move in the specified direction
+			// a.
+			// b.
+	// 3. return to the opposite or resetting
+
+	// INSTRUCTIONS
+	// hide all the slides
+
+	// show the first slide
+
+	// when the user clicks "next"
+
+		// hide the current slide
+		
+		// increment the slide index
+		slideIndex++; 
+			//same as slideIndex = slideIndex + 1
+
+		// conditional check of the slideIndex (if greater than 4...)
+
+			// reset to 0 
+
+		// set the current slide based on the newly incremented slideIndex 
+
+		// show the next 
+
+		// go to the start
+
+	// when the user clicks "previous"
+
+	// hide the current slide
+		
+		// increment the slide index
+		slideIndex++; 
+			//same as slideIndex = slideIndex + 1
+
+		// conditional check of the slideIndex (if greater than 4...)
+
+			// reset to 0 
+
+		// set the current slide based on the newly incremented slideIndex 
+
+		// show the next 
+
+	// close the document ready function
+
+		// go to the end
