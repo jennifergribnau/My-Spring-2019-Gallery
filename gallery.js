dc6013f0e3c5415357a8dfe1bd175f5d979a8d32
Jennifer Gribnau <jeg294@georgetown.edu>
2019-02-16 10:23:00 -0500

    added java for gallery

--- gallery.js
+++ gallery.js
@@ -1,15 +1,14 @@
 // document ready function
-$(document).ready(function(){}
-	// DEFINE YOUR VARIABLES
+$(document).ready(function(){
+	// VARIABLES
 	// select all the slides (value of the variable will be an array)
-var
-	// set the ndex slide (number) AKA where we start
-
+	// set the index slide (number) AKA where we start
+	var slides = ["#slide1", "#slide2", "#slide3", "#slide4", "#slide5"];
 
 	// current slide based on the index
-
+	var currentSlide = 0;
 	// STEPS
-	// 1. hide all slides except for the one we want to see (e.g all but the first)
+	// 1. hide all slides except for the one we want to see (e.g all but the first)		
 			// a.
 			// b.
 	// 2. move in the specified direction
@@ -19,17 +18,24 @@ var
 
 	// INSTRUCTIONS
 	// hide all the slides
-
-	// show the first slide
-
-	// when the user clicks "next"
-
-		// hide the current slide
+	for (var i=0; i<slides.length; i++)  {
+		$(slides[i]).hide();
+	}
 		
-		// increment the slide index
-		slideIndex++; 
-			//same as slideIndex = slideIndex + 1
+	// show the first slide
+	$(slides[currentSlide]).show();
+	// when the user clicks "next"
+	$("#nextButton").click(function(){
+		// hide the current slide
+		$(slides[currentSlide]).hide();
 
+		// increment the slide index
+		currentSlide++;
+		if (currentSlide > 4){
+			currentSlide = 0;
+			
+		}
+	//same as slideIndex = slideIndex + 1
 		// conditional check of the slideIndex (if greater than 4...)
 
 			// reset to 0 
@@ -37,15 +43,17 @@ var
 		// set the current slide based on the newly incremented slideIndex 
 
 		// show the next 
-
+		$(slides[currentSlide]).show();
+	});
 		// go to the start
 
 	// when the user clicks "previous"
-
+	$("prevButton").click(function(){
 	// hide the current slide
-		
+		$(slides[currentSlide]).hide();
 		// increment the slide index
-		slideIndex++; 
+		
+
 			//same as slideIndex = slideIndex + 1
 
 		// conditional check of the slideIndex (if greater than 4...)
@@ -53,9 +61,13 @@ var
 			// reset to 0 
 
 		// set the current slide based on the newly incremented slideIndex 
-
+		currentSlide--;
+		if (currentSlide < 0)
+			currentSlide = 4;
+	}
 		// show the next 
-
+		$(slides[currentSlide]).show();
+	}); 
 	// close the document ready function
 
 		// go to the end
